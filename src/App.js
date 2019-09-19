17a65754bce944e74a828d6b21a71143f7b525cf
Alexandre S. Soares <ale_santos.soares@hotmail.com>
2019-09-19 18:28:28 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { memo, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [movement, setMovement] = useState(0)
+  const [lastTouch, setLastTouch] = useState(0)
+
+  const handleTouchStart = e => {
+    const clientX = e.nativeEvent.targetTouches[0].clientX
+    return setLastTouch(clientX)
+  }
+
+  const handleMovement = (delta) => {
+    if (delta < 0) {
+      return setMovement(0)
+    }
+    return setMovement(84)
+  };
+
+  const handleTouchMove = e => {
+    const delta = lastTouch - e.nativeEvent.touches[0].clientX;
+    setLastTouch(e.nativeEvent.touches[0].clientX)
+    return handleMovement(delta)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div className="sliding">
+        <div 
+          className="slidingContent"
+          onTouchStart={handleTouchStart}
+          onTouchMove={handleTouchMove}
+          style={{
+            transform: `translateX(${movement * -1}px)`,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Your content here!
+        </div>
+        <div className="slidingOptions"
+          style={{
+            transform: `translateX(${movement * -1}px)`,
+          }}
+        >
+          Delete
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default memo(App);
